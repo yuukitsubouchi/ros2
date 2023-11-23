3dc5b60143bbbf8b10a69757083d34025ca777e2
Tsubo Yuki <tytt3462@gmail.com>
2023-11-23 12:22:17 +0900

    ros2

---
+++ install/mypkg/lib/python3.8/site-packages/mypkg/listener.py
@@ -0,0 +1,14 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person
+#from std_msgs.msg import Int16
+
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %s" % msg)#node.get_logger().info("Listen: %d" % msg.data)
+
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Person, "person", cb, 10)#pub = node.create_subscription(Int16, "countup", cb, 10)
+
+rclpy.spin(node)
