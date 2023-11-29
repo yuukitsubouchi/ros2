20cac23df7977c305f8c4f696ab76c1563fc48b9
Tsubo Yuki <tytt3462@gmail.com>
2023-11-30 04:07:01 +0900

    Add a command

--- src/mypkg/test/test.bash
+++ src/mypkg/test/test.bash
@@ -3,10 +3,10 @@
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
-cd $dir/ros2_ws
+cd $dir/ros2
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+ cat /tmp/mypkg.log |
+ grep 'Listen: 10'
