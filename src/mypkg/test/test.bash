315856d5a08c6461b88bfacb63956a37bc4bfa78
Tsubo Yuki <tytt3462@gmail.com>
2023-11-30 03:42:54 +0900

    test.bash

--- src/mypkg/test/test.bash
+++ src/mypkg/test/test.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-2
-3 dir=~
-4 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
-5
-6 cd $dir/ros2_ws
-7 colcon build
-8 source $dir/.bashrc
-9 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-10
-11 cat /tmp/mypkg.log |
-12 grep 'Listen: 10'
+
+dir=~
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
